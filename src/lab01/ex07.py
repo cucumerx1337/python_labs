34bbaf91ce80e692dab75e6424a79cc0ea62dc43
cucumerx1337 <89622110997r@gmail.com>
2026-09-11 23:13:31 +0300

    lab01 finish

--- src/lab01/ex07.py
+++ src/lab01/ex07.py
@@ -1,15 +1,26 @@
 stroka = input()
-out=''
-k=0
-c = 0
-start=0
-step=0
+out = ''
+k = 0
+fin = 0
+cs = 0
+start = 0
 for i in stroka:
-    print(str(i).upper)
-    c+=1
-    if i == i.upper:
-        print(i.upper)
-        out+=i
+    cs += 1
+    if i == i.upper() and i.isalpha() and k != 1:
+        out += i
+        k += 1
+        start = cs
+for i in range(len(stroka) - 1):
+    if stroka[i] in '0123456789':
+        out += stroka[i + 1]
+        fin = i
+        break
+step = (fin + 1) - (start - 1)
+for n in range(fin + 1 + step, len(stroka), step):
+    out += stroka[n]
+    if stroka[n] == '.':
+        break
 print(out)
-    
 
+
+    
