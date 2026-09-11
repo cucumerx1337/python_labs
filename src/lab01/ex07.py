78fe3553472ce96cc7a154b64eef55438bd375c3
cucumerx1337 <89622110997r@gmail.com>
2026-09-11 16:20:24 +0300

    lab01 ex7 no finish

---
+++ src/lab01/ex07.py
@@ -0,0 +1,15 @@
+stroka = input()
+out=''
+k=0
+c = 0
+start=0
+step=0
+for i in stroka:
+    print(str(i).upper)
+    c+=1
+    if i == i.upper:
+        print(i.upper)
+        out+=i
+print(out)
+    
+
